0e9c3770bb7efd3127c1fa0c13e23c4a00ed4780
高向孚 <Chibaibuki1@gaoxiangfudemacbook-pro.local>
2016-01-13 03:51:26 +0800

    Added a new class for data persistence.

---
+++ DesGo/Model/dataPersistence.h
@@ -0,0 +1,13 @@
+//
+//  dataPersistence.h
+//  DesGo
+//
+//  Created by 高向孚 on 16/1/13.
+//  Copyright © 2016年 ByStudio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface dataPersistence : NSObject
+
+@end
---
+++ DesGo/Model/dataPersistence.m
@@ -0,0 +1,13 @@
+//
+//  dataPersistence.m
+//  DesGo
+//
+//  Created by 高向孚 on 16/1/13.
+//  Copyright © 2016年 ByStudio. All rights reserved.
+//
+
+#import "dataPersistence.h"
+
+@implementation dataPersistence
+
+@end
