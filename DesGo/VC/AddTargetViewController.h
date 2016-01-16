71541481f671d6fbdf330aafa6d4ea27acc2d29a
高向孚 <Chibaibuki1@gaoxiangfudembp.lan>
2016-01-16 13:04:00 +0800

    add addTargetVc

---
+++ DesGo/VC/AddTargetViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddTargetViewController.h
+//  DesGo
+//
+//  Created by 高向孚 on 16/1/16.
+//  Copyright © 2016年 ByStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddTargetViewController : UIViewController
+
+@end
---
+++ DesGo/VC/AddTargetViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddTargetViewController.m
+//  DesGo
+//
+//  Created by 高向孚 on 16/1/16.
+//  Copyright © 2016年 ByStudio. All rights reserved.
+//
+
+#import "AddTargetViewController.h"
+
+@interface AddTargetViewController ()
+
+@end
+
+@implementation AddTargetViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
